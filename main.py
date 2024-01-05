65b6ff2deadc83e3f505fbdb571cd6ccbb0e4091
GearMic <mic.lp281@gmail.com>
2024-01-05 12:03:03 +0100

    binning is now done in py

--- main.py
+++ main.py
@@ -40,7 +40,15 @@ def correlation_function_alt(ensemble: np.ndarray, m: int, a: float, j: int = 0)
     return 1 / rows * correlation_sum
 
 
+def bin_normalized(data, n_bins, xlower, xupper):
+    rows, cols = data.shape
+    bin_size = (xupper - xlower) / n_bins
 
+    bins, edges = np.histogram(data, n_bins, (xlower, xupper))
+    bins_x = edges[:-1] + bin_size / 2
+    bins_y = bins / bin_size / (rows*cols)
+
+    return bins_x, bins_y
 
 
 
@@ -52,7 +60,6 @@ ms = 4
 ## Fig.4 (more or less)
 measurements = np.genfromtxt('harmonic_a.csv', delimiter=',')#, skip_header=5, usecols=data_usecols, converters=data_converters)
 rows, cols = measurements.shape
-print(measurements.shape)
 t = range(cols)
 
 for row in range(3):
@@ -63,18 +70,38 @@ plt.savefig(list_filename('plot/4/plot'), dpi=dpi)
 
 
 ## Fig. 5
-bins = np.genfromtxt('bins.csv', delimiter=',')
-bins_x, bins_y = bins[0], bins[1]
-bin_size = bins_x[1] - bins_x[0]
-bins_x += bin_size / 2 # for plotting bin value in the middle of each bin
-func_x = np.linspace(bins_x[0], bins_x[-1] + bin_size, 100)
+
+# bins = np.genfromtxt('bins.csv', delimiter=',')
+# bins_x, bins_y = bins[0], bins[1]
+# bin_size = bins_x[1] - bins_x[0]
+# bins_x += bin_size / 2 # for plotting bin value in the middle of each bin
+
+
+data = measurements
+# rows, cols = data.shape
+
+# bin_range = (-3.0, 3.0)
+# n_bins = 40
+
+# bin_size = (bin_range[1] - bin_range[0]) / n_bins
+# bins, edges = np.histogram(data, n_bins, bin_range)
+# bins_x = edges[:-1] + bin_size / 2
+# bins_y = bins / bin_size / (rows*cols)
+# print(edges)
+# print(bins_x)
+
+xlower, xupper = -3., 3.
+bins_x, bins_y = bin_normalized(data, 40, xlower, xupper)
+
+# func_x = np.linspace(bins_x[0], bins_x[-1] + bin_size, 100)
+func_x = np.linspace(xlower, xupper, 200)
 func_y = 0.59 * np.exp(-1.1 * func_x**2)
 
 plt.clf()
-plt.plot(bins_x, bins_y, 'x', ms=4)
-plt.plot(func_x, func_y)
-plt.title("OUTDATED DATA")
+plt.plot(bins_x, bins_y, 'x', ms=4)#, color='tab:red')
+plt.plot(func_x, func_y, lw=.75, color='tab:gray')
 plt.savefig(list_filename('plot/5/bins'), dpi=dpi)
+
 
 
 ## Fig. 6
