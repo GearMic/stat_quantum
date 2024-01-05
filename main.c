6f53178cebc54e66bd87e23b644f4ef869920de4
GearMic <mic.lp281@gmail.com>
2024-01-05 11:44:25 +0100

    Fig. 7 reproduced

--- main.c
+++ main.c
@@ -13,13 +13,16 @@ double epsilon;
 double a;
 double Delta;
 // double complex a = I * epsilon;
-const double m0 = 1.0;
+double m0;
 // potential
-const double mu = 1.0;
-const double lambda = 0.;
+double mu_sq;
+double lambda;
+double f_sq;
 // x range
 const double xlower = -2.;
 const double xupper = 2.;
+
+double (*potential_ptr)(double);
 
 
 
@@ -116,12 +119,17 @@ void bin_range(double range[], unsigned int N_bins, double xlower, double xupper
 // big functions
 double potential(double x)
 {
-    return 1./2. * pow(mu, 2) * pow(x, 2) + lambda * pow(x, 4); // anharmonic oscillator potential
+    return 1./2. * pow(mu_sq, 2) * pow(x, 2) + lambda * pow(x, 4); // anharmonic oscillator potential
+}
+
+double potential_alt(double x)
+{
+    return lambda * pow( pow(x, 2.f) - f_sq, 2.f );
 }
 
 double action_point(double x0, double x1)
 {
-    return epsilon * (1./2. * m0 * pow((x1-x0), 2) / pow(epsilon, 2) + potential(x0));
+    return epsilon * (1./2. * m0 * pow((x1-x0), 2) / pow(epsilon, 2) + (*potential_ptr)(x0));
 }
 
 double action(double* x, unsigned int N)
@@ -142,7 +150,7 @@ double action_2p(double xm1, double x0, double x1)
 
 double complex c_action_point(double x0, double x1)
 {
-    return a * (1./2. * m0 * cpow((x1-x0), 2) / cpow(a, 2) + potential(x0));
+    return a * (1./2. * m0 * cpow((x1-x0), 2) / cpow(a, 2) + (*potential_ptr)(x0));
 }
 
 double complex c_action_2p(double xm1, double x0, double x1)
@@ -182,7 +190,8 @@ void metropolis_algo(
     // double ensemble[N_lattices*(1+N_measure)][N+1], double equilibrium_ensemble[N_lattices][N+1])
 {
     // ensemble
-    const unsigned int N_measurements = N_lattices * (1 + N_measure);
+    // const unsigned int N_measurements = N_lattices * (1 + N_measure); // NOTE: for including the initial random lattice configurations
+    const unsigned int N_measurements = N_lattices * N_measure;
     double ensemble[N_measurements][N+1];
     double equilibrium_ensemble[N_lattices][N+1]; // array containing the "finished" states;
 
@@ -192,15 +201,18 @@ void metropolis_algo(
     x[0] = x0;
     x[N] = xN;
 
-    // measure initial lattice configuration
-    randomize_double_array(x+1, N-1, xlower, xupper);
-    memcpy(ensemble[0], x, (N+1)*sizeof(double));
+    // // measure initial lattice configuration
+    // randomize_double_array(x+1, N-1, xlower, xupper);
+    // memcpy(ensemble[0], x, (N+1)*sizeof(double));
 
     // metropolis algorithm
-    unsigned int measure_index = 1;
+    unsigned int measure_index = 0;
     for (int l=0; l<N_lattices; l++) {
         randomize_double_array(x+1, N-1, xlower, xupper);
-        
+        // // measure initial lattice configuration
+        // memcpy(ensemble[0], x, (N+1)*sizeof(double));
+        // measure_index++;
+
         for (int j=0; j<N_measure; j++) {
             for (int k=0; k<N_montecarlo; k++) {
                 for (int i=1; i<N; i++) {
@@ -240,13 +252,42 @@ int main()
     srand(time(NULL));
     // srand(42);
 
-
+    m0 = 1.0;
+    mu_sq = 1.0;
+    lambda = 0.0;
     N = 1000;
     epsilon = 1.;
     Delta = 2 * sqrt(epsilon);
-    metropolis_algo(0., 0., 1, 60, 5, 5, "out.csv", NULL);
+    potential_ptr = *potential;
+    metropolis_algo(0., 0., 3, 60, 5, 5, "harmonic_a.csv", NULL);
 
-    
+    m0 = 0.5;
+    mu_sq = 2.0;
+    lambda = 0.0;
+    N = 51;
+    epsilon = 0.5;
+    Delta = 2 * sqrt(epsilon);
+    metropolis_algo(0., 0., 3, 60, 5, 5, "harmonic_b.csv", NULL);
+    // metropolis_algo(0., 0., 6, 60, 5, 5, NULL, "harmonic_b.csv");
+
+
+    //// Fig. 7
+    m0 = 0.5;
+    lambda = 1.0;
+    epsilon = 1.0;
+    N = 50;
+    potential_ptr = *potential_alt;
+
+
+    f_sq = 0.5;
+    metropolis_algo(0., 0., 1, 1, 40, 5, "anharmonic_a.csv", NULL);
+    f_sq = 1.0;
+    metropolis_algo(0., 0., 1, 1, 40, 5, "anharmonic_b.csv", NULL);
+    f_sq = 2.0;
+    metropolis_algo(0., 0., 1, 1, 40, 5, "anharmonic_c.csv", NULL);
+
+
+
 /*
     // bin the data
     double bin_lower = -5.;
--- main.py
+++ main.py
@@ -15,25 +15,42 @@ def list_filename(name, suffix='.png'): # to save plots successively
 
         return filename
 
-def correlation_function(ensemble: np.ndarray, m: int):
+
+def correlation_function(ensemble: np.ndarray, m: int, a):
     rows, cols = ensemble.shape
-    a = 1.
 
     correlation_sum = 0
     for i in range(rows):
         row = ensemble[i]
         for j in range(cols-m):
-            correlation_sum += row[i] * row[i+m]
+            correlation_sum += row[j] * row[j+m]
 
     return 1 / rows / (cols-m) * correlation_sum
 
 
+def correlation_function_alt(ensemble: np.ndarray, m: int, a: float, j: int = 0):
+    rows, cols = ensemble.shape
+
+    correlation_sum = 0
+
+    for i in range(rows):
+        row = ensemble[i]
+        correlation_sum += row[j] * row[j+m]
+
+    return 1 / rows * correlation_sum
+
+
+
+
+
 
 dpi = 200
-
+ms = 4
 # ax = plt.gca()
 
-measurements = np.genfromtxt('out.csv', delimiter=',')#, skip_header=5, usecols=data_usecols, converters=data_converters)
+
+## Fig.4 (more or less)
+measurements = np.genfromtxt('harmonic_a.csv', delimiter=',')#, skip_header=5, usecols=data_usecols, converters=data_converters)
 rows, cols = measurements.shape
 print(measurements.shape)
 t = range(cols)
@@ -42,23 +59,11 @@ for row in range(3):
     # plt.plot(t, measurements[row])
     plt.plot(t, measurements[-row-1])
 
-# plt.ylim(-3, 3)
-
-# i=0
-# while True:
-#     filename = "plot/plot%i.png" % i
-#     if os.path.isfile(filename):
-#         i += 1
-#         continue
-
-#     plt.savefig(filename, dpi=dpi)
-#     break
-
-plt.savefig(list_filename('plot/plot'), dpi=dpi)
+plt.savefig(list_filename('plot/4/plot'), dpi=dpi)
 
 
-# filename_bins = "plot/bins.png"
-bins = np.genfromtxt('bins.csv', delimiter=',')#, skip_header=5, usecols=data_usecols, converters=data_converters)
+## Fig. 5
+bins = np.genfromtxt('bins.csv', delimiter=',')
 bins_x, bins_y = bins[0], bins[1]
 bin_size = bins_x[1] - bins_x[0]
 bins_x += bin_size / 2 # for plotting bin value in the middle of each bin
@@ -68,4 +73,58 @@ func_y = 0.59 * np.exp(-1.1 * func_x**2)
 plt.clf()
 plt.plot(bins_x, bins_y, 'x', ms=4)
 plt.plot(func_x, func_y)
-plt.savefig(list_filename('plot/bins'), dpi=dpi)
+plt.title("OUTDATED DATA")
+plt.savefig(list_filename('plot/5/bins'), dpi=dpi)
+
+
+## Fig. 6
+epsilon = 0.5
+
+data_b = np.genfromtxt('harmonic_b.csv', delimiter=',')
+if len(data_b.shape) == 1:
+    data_b = np.expand_dims(ensemble, 0)
+rows, cols = data_b.shape
+
+correlation_x = np.array(tuple(m*epsilon for m in range(cols)))
+correlation_y = np.array(tuple(correlation_function(data_b, m, epsilon) for m in range(cols)))
+# j = 1
+# correlation_y = np.array(tuple(correlation_function_alt(data_b, m, epsilon, 1) for m in range(cols-j)))
+# correlation_x = np.array(tuple(m*epsilon for m in range(cols-j)))
+
+fig, ax = plt.subplots()
+ax.set_yscale('log')
+ax.yaxis.set_major_formatter(plt.ScalarFormatter())
+plt.xlim(0.0, 3.0)
+# plt.clf()
+ax.plot(correlation_x, correlation_y, 'x', ms=4)
+plt.savefig(list_filename('plot/6/correlation'), dpi=dpi)
+
+
+#### Fig.7
+data_anharmonic_a = np.genfromtxt('anharmonic_a.csv', delimiter=',')
+data_anharmonic_b = np.genfromtxt('anharmonic_b.csv', delimiter=',')
+data_anharmonic_c = np.genfromtxt('anharmonic_c.csv', delimiter=',')
+data_anharmonic = (data_anharmonic_a, data_anharmonic_b, data_anharmonic_c)
+if len(data_anharmonic_a.shape) > 1: print("Obacht")
+f_sq_tup = (0.5, 1.0, 2.0)
+
+N = len(data_anharmonic_a)
+plt.clf()
+fig, axs = plt.subplots(1, 3)
+
+for i in range(len(axs)):
+    ax = axs[i]
+    data = data_anharmonic[i]
+
+    # f = np.sqrt(f_sq_tup[i])
+    f_sq = f_sq_tup[i]
+    f = np.sqrt(f_sq)
+
+    ax.vlines((-f, f), 0, N+1, linestyles='dashed', color='tab:blue')
+    ax.plot(data, range(N), lw=.75, color='tab:gray')#, linestyles='dash-dotted')
+    ax.plot(data, range(N), 'x', ms=ms, color='tab:red')#, linestyles='dash-dotted')
+    ax.set_ylim(0.0, N+1)
+    ax.set_title('$f^2 = %.1f$' % f_sq)
+
+fig.suptitle('Fig. 7')
+fig.savefig(list_filename('plot/7a/anharmonic_a'), dpi=dpi)
